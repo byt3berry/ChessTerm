01fbbd72c67e59858fa83af224df878189c012ed
byt3berry <0xm.this@gmail.com>
2025-03-03 21:19:29 +0100

    Start implementing pinned piece

--- src/pieces/bishop.rs
+++ src/pieces/bishop.rs
@@ -74,9 +74,11 @@ mod tests {
     use crate::board::Board;
     use crate::board::board_builder::BoardBuilder;
     use crate::pieces::color::Color;
+    use crate::pieces::king::King;
     use crate::pieces::move_struct::MoveKind;
     use crate::pieces::pawn::Pawn;
     use crate::pieces::piece_kind::PieceKind;
+    use crate::pieces::rook::Rook;
     use crate::pieces::{Move, Piece};
 
     use super::Bishop;
@@ -157,6 +159,57 @@ mod tests {
         expected.insert(Move::new((3isize, 3isize).into(), (2isize, 4isize).into(), MoveKind::Attack));
         expected.insert(Move::new((3isize, 3isize).into(), (4isize, 2isize).into(), MoveKind::Attack));
         expected.insert(Move::new((3isize, 3isize).into(), (4isize, 4isize).into(), MoveKind::Attack));
+
+        let possible_moves: HashSet<Move> = piece.possible_moves(&board);
+
+        assert_eq!(
+            expected,
+            possible_moves,
+            "\nelements expected missing: {:?}\nelements not expected: {:?}",
+            expected.difference(&possible_moves),
+            possible_moves.difference(&expected),
+            );
+    }
+
+    #[test]
+    fn test_pinned_no_move() {
+        let board: Board = BoardBuilder::new()
+            .add(PieceKind::BISHOP(Bishop::new((3isize, 3isize).into(), Color::BLACK)))
+            .add(PieceKind::KING(King::new((0isize, 3isize).into(), Color::BLACK)))
+            .add(PieceKind::ROOK(Rook::new((5isize, 3isize).into(), Color::WHITE)))
+            .build();
+        let piece: &PieceKind = board
+            .piece((3isize, 3isize).into())
+            .expect("The piece {position} should exist");
+        let expected: HashSet<Move> = HashSet::new();
+
+        let possible_moves: HashSet<Move> = piece.possible_moves(&board);
+
+        assert_eq!(
+            expected,
+            possible_moves,
+            "\nelements expected missing: {:?}\nelements not expected: {:?}",
+            expected.difference(&possible_moves),
+            possible_moves.difference(&expected),
+            );
+    }
+
+    #[test]
+    fn test_pinned_can_move() {
+        let board: Board = BoardBuilder::new()
+            .add(PieceKind::KING(King::new((0isize, 0isize).into(), Color::BLACK)))
+            .add(PieceKind::BISHOP(Bishop::new((1isize, 1isize).into(), Color::BLACK)))
+            .add(PieceKind::BISHOP(Bishop::new((6isize, 6isize).into(), Color::WHITE)))
+            .build();
+        let piece: &PieceKind = board
+            .piece((1isize, 1isize).into())
+            .expect("The piece {position} should exist");
+        let mut expected: HashSet<Move> = HashSet::new();
+        expected.insert(Move::new((1isize, 1isize).into(), (2isize, 2isize).into(), MoveKind::Attack));
+        expected.insert(Move::new((1isize, 1isize).into(), (3isize, 3isize).into(), MoveKind::Attack));
+        expected.insert(Move::new((1isize, 1isize).into(), (4isize, 4isize).into(), MoveKind::Attack));
+        expected.insert(Move::new((1isize, 1isize).into(), (5isize, 5isize).into(), MoveKind::Attack));
+        expected.insert(Move::new((1isize, 1isize).into(), (6isize, 6isize).into(), MoveKind::Attack));
 
         let possible_moves: HashSet<Move> = piece.possible_moves(&board);
 
