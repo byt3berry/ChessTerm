3b541f8cfc7c19d09bebff9652e9e0f3ada155c9
byt3berry <0xm.this@gmail.com>
2025-03-12 20:27:42 +0100

    Refactor Square, don't need a named field

--- src/game/board/square.rs
+++ src/game/board/square.rs
@@ -3,34 +3,32 @@ use crate::game::pieces::piece_kind::PieceKind;
 use super::color::Color;
 
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
-pub(crate) struct Square {
-    piece: Option<PieceKind>,
-}
+pub(crate) struct Square (Option<PieceKind>);
 
 impl Square {
     pub(super) const fn set_piece(&mut self, piece: PieceKind) {
-        self.piece = Some(piece);
+        self.0 = Some(piece);
     }
 
     pub(super) const fn piece_unset(&mut self) -> PieceKind {
-        assert!(self.piece.is_some());
+        assert!(self.0.is_some());
 
-        let piece: PieceKind = self.piece.unwrap();
-        self.piece = None;
+        let piece: PieceKind = self.0.unwrap();
+        self.0 = None;
         piece
     }
 
     pub(crate) fn piece(&self, color: Color) -> Option<&PieceKind> {
-        if color == Color::Any || self.piece.is_some_and(|piece| piece.color() == color) {
-            return self.piece.as_ref();
+        if color == Color::Any || self.0.is_some_and(|piece| piece.color() == color) {
+            return self.0.as_ref();
         }
         
         None
     }
 
     pub(super) fn piece_mut(&mut self, color: Color) -> Option<&mut PieceKind> {
-        if color == Color::Any || self.piece.is_some_and(|piece| piece.color() == color) {
-            return self.piece.as_mut();
+        if color == Color::Any || self.0.is_some_and(|piece| piece.color() == color) {
+            return self.0.as_mut();
         }
         
         None
@@ -40,8 +38,6 @@ impl Square {
 #[cfg(test)]
 impl Square {
     pub(super) const fn new(piece: Option<PieceKind>) -> Self {
-        Self {
-            piece,
-        }
+        Self(piece)
     }
 }
